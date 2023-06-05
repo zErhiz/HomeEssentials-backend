fe24d0ff2e50014db503126fec1e571435c74455
Mateo Carrizo Flores <mateocarrizoflores@gmail.com>
2023-06-04 21:21:37 -0300

    dev

--- controllers/auth/read.js
+++ controllers/auth/read.js
@@ -1,7 +1,7 @@
 let name = 'auth'
 
 let read = (req, res, next) => res.status(200).render(
-  'index', //nombre de la vista
+  'index', //nombre de la vaista
   {
     title: '/' + name,
     subtitle: 'Endpoint of ' + name
