fdbc4c6d4c351510bfb62f7c7e9e69aaab554233
Sebastian patricio ovalle <127897018+Sebas-ovalle@users.noreply.github.com>
2023-06-06 13:53:55 -0300

    nodemailer (#3)

--- controllers/auth/signUp.js
+++ controllers/auth/signUp.js
@@ -1,30 +1,35 @@
-//import createHttpError from "http-errors";
-import crypto from "crypto";
-import bcryptjs from "bcryptjs";
+import crypto from 'crypto'
+import bcryptjs from 'bcryptjs'
 import User from "../../models/User.js";
+import createEmailTransporter from '../../config/nodemailer.js';
 
 
-let create = async (req, res, next) => {
-  req.body.role = 0;
-  req.body.is_online = false;
-  req.body.is_verified = false;
-  req.body.verify_code = crypto.randomBytes(10).toString("hex");
-  req.body.password = bcryptjs.hashSync(req.body.password, 10);
-  try {
-    let one = new User(req.body);
+let create = async(req, res, next) => {
+    console.log(req.file);
+    const {firebaseUrl} = req.file ? req.file : '';
 
-   
-    await one.save();
-    return res.status(201).json({
-      message: "user created!",
-      user: one.email,
-      success: true,
-      timestamps: one.createdAt,
-    });
-  } catch (error) {
-    console.log(error);
-    next(error);
-  }
-};
+    req.body.photo = firebaseUrl
+    req.body.role = 0;
+    req.body.is_online = false;
+    req.body.is_verified = false;
+    req.body.verify_code = crypto.randomBytes(10).toString('hex');
+    req.body.password = bcryptjs.hashSync(req.body.password, 10)
+      try {
+        let one = new User(req.body)
+        createEmailTransporter(one)//envio del mail para verificacion
+          .then(info => console.log(info))
+          .catch(error => console.log(error))
+        await one.save()
+        return res.status(201).json({
+          message: 'user created successfully!!',
+          user: one.email,
+          success: "Ok",
+          timestamps: one.createdAt  
+        })
+      } catch (error) {
+        console.log(error)
+        next(error)
+      }
+}
 
 export default create;
