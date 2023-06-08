1519797c691f9e9a17d748d3ad85078ae0eb39d4
Mateo Carrizo Flores <mateocarrizoflores@gmail.com>
2023-06-08 15:58:18 -0300

    first

--- routes/product.js
+++ routes/product.js
@@ -2,9 +2,11 @@ import  { Router }  from 'express';
 import read from '../controllers/products/read.js'
 import productsPaginationHome from "../controllers/products/best_sellers.js"
 import  searchProducts  from '../controllers/products/search_products.js';
+import getProductsByCategory from '../controllers/products/getproductsbycategory.js';
 let router = Router()
 
 router.get('/', read);
 router.get('/sixcards', productsPaginationHome)
 router.get('/search',searchProducts)
+router.get('/categories/:categoryId',getProductsByCategory)
 export default router
