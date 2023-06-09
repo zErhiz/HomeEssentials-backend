481c457e235299137b3f80b253c38049292de9f9
lucas271102 <lucas001spe@gmail.com>
2023-06-09 12:06:38 -0300

    fixUser

--- models/User.js
+++ models/User.js
@@ -2,15 +2,15 @@ import { Schema, model } from "mongoose";
 
 let schema = new Schema(
   {
-    name: { type: String, required: true },
-    lastName: { type: String, required: true },
+    name: { type: String, required: false },
+    lastName: { type: String, required: false },
     email: { type: String, required: true },
     password: { type: String, required: true },
     photo: { type: String, required: true },
     role: { type: Number, required: true },
     is_online: { type: Boolean, required: true },
-    is_verified: { type: Boolean, required: true },
-    verify_code: { type: String, required: true },
+    is_verified: { type: Boolean, required: false },
+    verify_code: { type: String, required: false },
   },
   {
     timestamps: true,
