069b4f449b61c6e4de08629bdab84c2c12d20325
Maximiliano Gartner <maxi-gartner@hotmail.com>
2023-06-09 18:05:28 -0300

    changes model products and changes sign in

--- models/Product.js
+++ models/Product.js
@@ -4,6 +4,8 @@ let schema = new Schema ({
     name: {type: String, required: true},
     description: {type: String, required: true},
     price: {type: Number, required: true},
+    price_offer: {type: Number, required: false},
+    percentage_offer: {type: Number, required: true},
     photo: { type: String, required: true},
     stock_Available: {type: Number, required: true},
     category_id: {type: Types.ObjectId, ref: 'categories',required: true},
