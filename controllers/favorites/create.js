2a80734d0ba7b50ba656685406cc00bc8b9239ab
Maximiliano Gartner <113448691+maxi-gartner@users.noreply.github.com>
2023-06-12 21:41:00 -0300

    Controladores favoritos (#23)
    * add controller confirm cart
    
    * create controlers favorites

--- controllers/favorites/create.js
+++ controllers/favorites/create.js
@@ -1,12 +1,24 @@
 import Favorites from '../../models/Favorites.js'
+import User from '../../models/User.js';
 
 let create = async(req, res, next) => {
     try{
-        let one = await Favorites.create(req.body)
-        return res.status(201).json({
-            Favorites: one,
-            success: true
-        })
+        const { userEmail, productId } = req.body;
+        let user = await User.findOne({ email: userEmail, });
+        let article = await Favorites.findOne({ user_id: user._id, product_id: productId });
+        if(!article){
+            let data = {user_id: user._id, product_id: productId}
+            let one = await Favorites.create(data)
+            return res.status(201).json({
+                Favorites: one,
+                success: true
+            })
+        }else{
+            return res.status(400).json({
+                message: 'You already added this product to your favorites'
+            })
+        }
+        
     } catch(error){
         next(error)
     }
